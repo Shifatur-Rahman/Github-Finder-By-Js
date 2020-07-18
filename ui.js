008c7190de6c72ba4b308ef41d46f59c0d8c7505
Shifatur-Rahman <Shifatur-Rahman>
2020-07-18 20:45:52 +0600

    fifth commit

--- ui.js
+++ ui.js
@@ -4,16 +4,16 @@ class UI {
   }
 
   showProfile(user) {
-    this.profile.innerHTML = (
+    this.profile.innerHTML = `
       <div class='card card-body'>
         <div class='row'>
           <div class='col-md-3'>
-            <img class='img-fluid mb-2' src={user.avatar_url} />
-            <a src={user.html_url}></a>
+            <img class='img-fluid mb-2' src="${user.avatar_url}" />
+            <a class="btn btn-primary btn-block mb-4" src="${user.html_url}" target="_blank">View Profile</a>
           </div>
           <div class='col-md-9'></div>
         </div>
       </div>
-    );
+    `;
   }
 }
