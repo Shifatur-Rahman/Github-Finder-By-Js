089c0aba0364b65918dedcbc98a93efde1223b41
Shifatur-Rahman <Shifatur-Rahman>
2020-07-19 10:23:22 +0600

    commit

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const github = new Github(); //  github.js er class declare korlam
-const ui = new UI();
+const ui = new UI(); //ui.js r class declare korlam
 const searchInput = document.getElementById("search-input");
 
 searchInput.addEventListener("keyup", (e) => {
   const searchText = e.target.value;
-  if (searchInput !== "") {
+  if (searchText !== "") {
     github.getUser(searchText).then((data) => {
       if (data.profile.message === "Not Found") {
         // show alert
+        ui.showAlert("User Not found", "alert alert-danger");
       } else {
         // show profile
         ui.showProfile(data.profile);
       }
     });
   } else {
+    ui.clearProfile();
   }
 });
--- ui.js
+++ ui.js
@@ -2,18 +2,67 @@ class UI {
   constructor() {
     this.profile = document.getElementById("profile");
   }
-
+  //display profile
   showProfile(user) {
     this.profile.innerHTML = `
       <div class='card card-body'>
         <div class='row'>
           <div class='col-md-3'>
-            <img class='img-fluid mb-2' src="${user.avatar_url}" />
-            <a class="btn btn-primary btn-block mb-4" src="${user.html_url}" target="_blank">View Profile</a>
+            <img class='img-fluid mb-2' src='${user.avatar_url}' />
+            <a
+              class='btn btn-primary btn-block mb-4'
+              src='${user.html_url}'
+              target='_blank'
+            >
+              View Profile
+            </a>
           </div>
-          <div class='col-md-9'></div>
+          <div class='col-md-9'>
+            <span class='badge badge-primary'>
+              Public Repos : ${user.repos_url}
+            </span>
+            <span class='badge badge-secondary'>
+              Gists Repos : ${user.gists_url}
+            </span>
+            <span class='badge badge-success'>
+              Following : ${user.following_url}
+            </span>
+            <span class='badge badge-info'>
+              Followers : ${user.followers_url}
+            </span>
+
+            <ul class='list-group mt-4'>
+              <li class='list-group-item'>Company : ${user.company}</li>
+              <li class='list-group-item'>Website/Blog : ${user.blog}</li>
+              <li class='list-group-item'>Location : ${user.location}</li>
+             
+            </ul>
+          </div>
         </div>
       </div>
-    `;
+  `;
+  }
+  // clear profile
+  clearProfile() {
+    this.profile.innerHTML = "";
+  }
+  showAlert(msg, className) {
+    const div = document.createElement("div");
+    div.className = className;
+    div.innerHTML = msg;
+    const mainContainer = document.querySelector(".main-container");
+    const search = document.querySelector(".search");
+    mainContainer.insertBefore(div, search);
+
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const alert = document.querySelector(".alert");
+    if (alert) {
+      alert.remove();
+    }
   }
 }
